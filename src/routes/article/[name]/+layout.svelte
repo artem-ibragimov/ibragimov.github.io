a9068e6a216f792ff31019c151ad7404482ac8f8
artem.ibragimov <artem.ibragimov@gmail.com>
2024-05-21 19:32:53 +0300

    Auto-commit

---
+++ src/routes/article/[name]/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<Header />
+<main class="Layout">
+	<slot />
+</main>
+
+<style lang="less" scoped>
+	.Layout {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100vw;
+		min-height: 100vh;
+		padding: 16px;
+	}
+</style>
